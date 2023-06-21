465d1b31676ff389e85179d0c16040a7c7ed2777
Tetra Silverberg <tetra.silv@gmail.com>
2023-06-20 20:39:46 -0400

    a

--- alternesting.py
+++ alternesting.py
@@ -1,2 +1,2 @@
-def n(w,f):f=~f;return w if len(w)<3else w[0]+"[("[f]+n(w[1:-1],f)+"])"[f]+w[-1]
+def n(w,f):f=~f;return w if len(w)<3 else w[0]+"[("[f]+n(w[1:-1],f)+"])"[f]+w[-1]
 print(f"({n(input(),1)})")
--- switch-case-tuple.py
+++ switch-case-tuple.py
@@ -1,6 +1,6 @@
 x = int(input())
-case = (("x <  5", "less than five"), 
+case = [("x <  5", "less than five"), 
         ("x >= 5", "greater than or equal to five"), 
-        ("x == 5", "is five") )
+        ("x == 5", "is five")]
 
 print(next(x[0][1]for x in[*zip(case,(*map(lambda c:eval(c[0]),case),))]if x[1]))
